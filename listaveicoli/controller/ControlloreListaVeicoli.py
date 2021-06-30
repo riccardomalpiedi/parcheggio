9c9f249952370b2b4231dbd3cb7d2727f54801ea
riccardomalpiedi <S1092879@studenti.univpm.it>
2021-06-30 18:27:12 +0200

    vista home

--- listaveicoli/controller/ControlloreListaVeicoli.py
+++ listaveicoli/controller/ControlloreListaVeicoli.py
@@ -8,15 +8,15 @@ class ControlloreListaVeicoli():
     def __init__(self):
         super(ControlloreListaVeicoli, self).__init__()
         self.model = ListaVeicoli()
-        if os.path.isfile('ListaVeicoli/data/lista_veicoli_salvata.pickle'):
+        if os.path.isfile('listaveicoli/data/lista_veicoli_salvata.pickle'):
             print("esiste")
-            with open('ListaVeicoli/data/lista_veicoli_salvata.pickle', 'rb') as f:
+            with open('listaveicoli/data/lista_veicoli_salvata.pickle', 'rb') as f:
                 lista_veicoli_salvata = pickle.load(f)
             self.model = lista_veicoli_salvata
 
     def aggiungi_veicolo(self, veicolo):
         self.model.aggiungi_veicolo(veicolo)
-        with open('ListaVeicoli/data/lista_veicoli_salvata.pickle', 'wb') as handle:
+        with open('listaveicoli/data/lista_veicoli_salvata.pickle', 'wb') as handle:
             pickle.dump(self.model, handle, pickle.HIGHEST_PROTOCOL)
 
     def get_lista_dei_veicoli(self):
@@ -27,7 +27,7 @@ class ControlloreListaVeicoli():
 
     def elimina_veicolo_by_id(self, id):
         self.model.rimuovi_veicolo_by_id(id)
-        with open('ListaVeicoli/data/lista_veicoli_salvata.pickle', 'wb') as handle:
+        with open('listaveicoli/data/lista_veicoli_salvata.pickle', 'wb') as handle:
             pickle.dump(self.model, handle, pickle.HIGHEST_PROTOCOL)
 
     def save_data(self):
