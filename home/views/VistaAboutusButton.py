e58d0765d131492880c9839e1ce8d35c5147c64e
RiccardoVico <s1094005@studenti.univpm.it>
2021-07-06 18:29:18 +0200

    Miglioramento Interfaccia Grafica pt.2

--- home/views/VistaAboutusButton.py
+++ home/views/VistaAboutusButton.py
@@ -1,7 +1,12 @@
-from PyQt5.QtWidgets import QGridLayout, QWidget
+from PyQt5.QtWidgets import QGridLayout, QWidget, QDialog
+from PyQt5.uic import loadUi
 
 
-class VistaAboutusButton(QWidget):
+class VistaAboutusButton(QDialog):
     def __init__(self):
         super(VistaAboutusButton, self).__init__()
-        gridlayout = QGridLayout()
+        loadUi("aboutus.ui", self)
+        self.back_button.clicked.connect(self.go_back_button)
+
+    def go_back_button(self):
+        self.close()
--- home/views/VistaHelpButton.py
+++ home/views/VistaHelpButton.py
@@ -1,7 +1,12 @@
-from PyQt5.QtWidgets import QGridLayout, QWidget
+from PyQt5.QtWidgets import QGridLayout, QWidget, QDialog
+from PyQt5.uic import loadUi
 
 
-class VistaHelpButton(QWidget):
+class VistaHelpButton(QDialog):
     def __init__(self):
         super(VistaHelpButton, self).__init__()
-        gridlayout = QGridLayout()
+        loadUi("help.ui", self)
+        self.back_button.clicked.connect(self.go_back_button)
+
+    def go_back_button(self):
+        self.close()
