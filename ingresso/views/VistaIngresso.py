617d94c9068137d3695a1ab09e8767317e4a6cb6
riccardomalpiedi <S1092879@studenti.univpm.it>
2021-07-14 21:46:04 +0200

    modifiche ingresso veicolo

--- ingresso/views/VistaIngresso.py
+++ ingresso/views/VistaIngresso.py
@@ -1,3 +1,7 @@
+import os
+import pickle
+
+from PyQt5.QtGui import QStandardItem, QStandardItemModel
 from PyQt5.QtWidgets import QWidget
 from PyQt5.uic import loadUi
 
@@ -9,15 +13,30 @@ class VistaIngresso(QWidget):
     def __init__(self):
         super(VistaIngresso, self).__init__()
         self.vista_lista_veicoli = VistaListaVeicoli()
-        self.controller = self.vista_lista_veicoli.controller
-        loadUi("Ui_Ingresso.ui", self)
-        self.inserisci_veicolo_button.clicked.connect(self.go_vista_inserisci_veicolo)
+        loadUi("Ingresso.ui", self)
+
+        self.comboveicoli_model = QStandardItemModel(self.comboBox)
+        if os.path.isfile('listaveicoli/data/lista_veicoli_salvata.pickle'):
+            with open('listaveicoli/data/lista_veicoli_salvata.pickle', 'rb') as f:
+                self.lista_veicoli_salvata = pickle.load(f)
+            for veicolo in self.lista_veicoli_salvata:
+                item = QStandardItem()
+                item.setText(veicolo.targa)
+                item.setEditable(False)
+                font = item.font()
+                font.setPointSize(18)
+                item.setFont(font)
+                self.comboveicoli_model.appendRow(item)
+            self.comboBox.setModel(self.comboveicoli_model)
+
+        self.pushButton.clicked.connect(self.go_vista_inserisci_veicolo)
 
     def go_vista_inserisci_veicolo(self):
-        self.vista_inserisci_veicolo = VistaInserisciVeicolo(self.controller, self.vista_lista_veicoli.update_ui)
+        self.vista_inserisci_veicolo = VistaInserisciVeicolo(self.vista_lista_veicoli.controller,
+                                                             self.vista_lista_veicoli.update_ui)
         self.vista_inserisci_veicolo.show()
 
     def closeEvent(self, event):
         print("ON CLOSE")
-        self.controller.save_data()
+        self.vista_lista_veicoli.controller.save_data()
         event.accept()
