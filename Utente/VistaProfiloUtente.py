fbc64669d91b9ea9ce90a8a092bfcc828551985a
RiccardoVico <s1094005@studenti.univpm.it>
2021-07-22 23:05:10 +0200

    Vista Profilo Utente

--- Utente/VistaProfiloUtente.py
+++ Utente/VistaProfiloUtente.py
@@ -1,7 +1,42 @@
-from PyQt5.QtWidgets import QWidget, QGridLayout
+from PyQt5.QtWidgets import QDialog
+from PyQt5.uic import loadUi
 
 
-class VistaProfiloUtente(QWidget):
+class VistaProfiloUtente(QDialog):
     def __init__(self):
         super(VistaProfiloUtente, self).__init__()
-        grid_layout = QGridLayout()
+        loadUi("Utente/VistaProfiloUtente.ui", self)
+
+        # self.nome_label.setText(
+        #     "<font color='white'>" + self.controller.get_nome_cliente() + " " + self.controller.get_cognome_cliente())
+        # self.username_label.setText("<font color='white'>Username: " + self.controller.get_username_cliente())
+        # self.codice_fiscale_label.setText("<font color='white'>Codice Fiscale: " + self.controller.get_cf_cliente())
+        # self.indirizzo_label.setText("<font color='white'>Indirizzo: " + self.controller.get_indirizzo_cliente())
+        # self.email_label.setText("<font color='white'>Email: " + self.controller.get_email_cliente())
+        # self.telefono_label.setText("<font color='white'>Telefono: " + self.controller.get_telefono_cliente())
+        # self.veicolo_label.setText("<font color='white'>Targa Veicolo: " + self.controller.get_veicolo_cliente())
+
+        self.update_button.clicked.connect(self.go_update_function)
+        self.gestione_veicoli_button.clicked.connect(self.go_gestione_veicoli_function)
+        self.gestione_prenotazioni_button.clicked.connect(self.go_gestisci_prenotazioni_function)
+        self.modifica_profilo_button.clicked.connect(self.go_modifica_profilo_function)
+        self.cambia_password_button.clicked.connect(self.go_modifica_password)
+
+        self.setWindowTitle("Profilo Utente")
+        self.setFixedWidth(self.width())
+        self.setFixedHeight(self.height())
+
+    def go_update_function(self):
+        pass
+
+    def go_gestione_veicoli_function(self):
+        pass
+
+    def go_gestisci_prenotazioni_function(self):
+        pass
+
+    def go_modifica_profilo_function(self):
+        pass
+
+    def go_modifica_password(self):
+        pass
